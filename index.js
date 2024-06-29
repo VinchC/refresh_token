65a9a2483b6192237c15fb363cd591f88f8ebe2c
Vincent C <vinch.cm@gmail.com>
2024-06-29 12:33:29 +0200

    upload file added

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const multer = require("multer");
 
 const app = express();
 
@@ -84,3 +85,30 @@ app.post("/refresh", (req, res) => {
     return res.status(400).send("Invalid refresh token.");
   }
 });
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 1000000 },
+  fileFilter: (req, file, cb) => {
+    if (
+      file.mimetype === "image/jpeg" ||
+      file.mimetype === "image/png" ||
+      file.mimetype === "image/jpg"
+    ) {
+      cb(null, true);
+    } else {
+      cb(new Error("Invalid file type"));
+    }
+  },
+});
+
+app.post("/upload", upload.single("file"), (req, res) => {});
